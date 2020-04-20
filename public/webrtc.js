bf1cb7b75c527cb71e9021f16a9100a8ff254bcb
sridharNT11 <sridharan.r@numerotec.com>
2020-04-20 18:35:16 +0530

    v3.3

--- public/webrtc.js
+++ public/webrtc.js
@@ -9,6 +9,11 @@ var peerConnectionConfig = {
     'iceServers': [
         {'urls': 'stun:stun.services.mozilla.com'},
         {'urls': 'stun:stun.l.google.com:19302'},
+        {
+          url: 'turn:numb.viagenie.ca',
+          credential: 'sridharan',
+          username: 'sridharan.r@numerotec.com'
+        }
     ]
 };
 
